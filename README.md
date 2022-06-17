a9e71de2cf8e871fb7438ba325d3bae277ce1467
Theo <tdgreer1203@gmail.com>
2022-06-17 04:27:19 -0400

    Idea for Structure

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# Password Generator Starter Code
+# Password Generator
+
+## Purpose
+
+The purpose of this assignement is to design a password generator that generates passwords based on input received from the user via window prompts. 
+
+## Built With
+
+* HTML
+* CSS
+* Javascript
+
+## Website
+
+Website will go here
+
+## Contribution
+
+Made by Theo Greer
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,52 @@
 // Assignment code here
+//Create  Constant Variables Here
+const upperCasedCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const lowerCasedCharacters = 'abcdefghijklmnopqrstuvwxyz';
+const numberCharacters = '1234567890';
+const specialCharacters = ' !"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~';
 
+//Create All Other Variables
+
+
+function generatePassword() {
+  //Prompt for length
+
+
+  //Prompt for includeUpperCased (if yes, add one upper-cased character now & subtract 1 from the length)
+
+
+  //Prompt for includeLowerCased (if yes, add one lower-cased character now & subtract 1 from the length)  
+
+
+  //Prompt for includeNumbers (if yes, add one number now & subtract 1 from the length)
+
+
+  //Prompt for includeSpecialCharacters (if yes, add one special character now & subtract 1 from the length)
+
+
+  //Generate rest of password
+  
+  //Create Functions
+  function generateNumber(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+  function returnUpperCasedCharacter(index) {
+    return upperCasedCharacters.charAt(index);
+  }
+
+  function returnLowerCasedCharacter(index) {
+    return lowerCasedCharacters.charAt(index);
+  }
+
+  function returnSpecialCharacter(index) {
+    return specialCharacters.charAt(index);
+  }
+
+  function scramblePassword(password) {
+
+  }
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
